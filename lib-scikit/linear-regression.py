7d60bc9fc01ee4cc708e201b59992906efd3f906
amir <amir.h.rassafi@gmail.com>
2023-05-06 16:00:06 +0330

    Fixed

--- lib-scikit/linear-regression.py
+++ lib-scikit/linear-regression.py
@@ -12,20 +12,21 @@ Y = dimensions.iloc[:, 4].values
 
 Y = np.where(Y == 'mid', 0, 1)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42, shuffle=True)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.6, random_state=15, shuffle=True)
 
 
 model = LinearRegression()
 model.fit(X_train, Y_train)
 Y_pred = model.predict(X_test)
 
+Y_pred_round = [0 if i < 0.5 else 1 for i in Y_pred]
 
 mse = mean_squared_error(Y_test, Y_pred)
-accuracy = accuracy_score(Y_test, Y_pred)
-print(accuracy, mse)
 
+accuracy = accuracy_score(Y_test, Y_pred_round)
 
-cm = confusion_matrix(Y_test, Y_pred)
+cm = confusion_matrix(Y_test, Y_pred_round)
+
 
 plt.matshow(cm)
 plt.title('Confusion Matrix')
@@ -34,4 +35,4 @@ plt.ylabel('True label')
 plt.xlabel('Predicted label')
 plt.show()
 
-print(classification_report(Y_test, Y_pred))
+print(classification_report(Y_test, Y_pred_round))
