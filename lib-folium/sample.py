d79195153e94b82bf8dc84b4e5eadd529e0c3a2c
Amir H. Rassafi <amir.h.rassafi@gmail.com>
2023-05-16 11:25:02 +0330

    Done

--- lib-folium/sample.py
+++ lib-folium/sample.py
@@ -4,23 +4,31 @@ from cluster_drawer import GeoPoint, Cluster, drawClusters
 from ride import Ride, generate_random_ride
 
 # Define the bounding box for Tehran (min_lat, max_lat, min_lng, max_lng)
-tehran_bbox = (35.65, 35.75, 51.24, 51.55)
+tehran_bbox = (35.65, 35.75, 51.20, 51.50)
 
 ride_count = 40
 
-dc_count = 6
+dc_count = 4
 
 def generate_random_coordinate(bbox):
     lat = random.uniform(bbox[0], bbox[1])
     lng = random.uniform(bbox[2], bbox[3])
     return GeoPoint(lat, lng)
 
-def getMapWithDcs(dcs, box):
+def getMapObject(box):
     map_center = GeoPoint((box[0] + box[1]) / 2, (box[2] + box[3]) / 2)        
     map_obj = folium.Map(location=map_center.toTuple(), zoom_start=12)
-    for dc in dcs:
-        folium.Marker(location=dc.toTuple(), icon=folium.Icon(color="red")).add_to(map_obj)
     return map_obj
+
+def drawDc(dcs, map):
+    for dc in dcs:
+        folium.Marker(location=dc.toTuple(), icon=folium.Icon(color="red")).add_to(map)
+
+def drawClustersWithDcs(name, clusters, dcs, box):
+    map_obj = getMapObject(box)
+    drawClusters(clusters, map_obj, name=name)
+    drawDc(dcs, map_obj)
+    map_obj.save(name + ".html")
 
 rides = [generate_random_ride(tehran_bbox) for i in range(ride_count)]
 sourcePoints = [ride.getSource() for ride in rides]
@@ -38,13 +46,17 @@ for i, ride in enumerate(rides):
     cluster.addPoint(ride.getDest())
     p2p_clusters.append(cluster)
 
-source_clusters = []
 dest_clusters = []
 for dc in dcs:
     dc_cluster = Cluster("dc_cluster")
     dc_cluster.setCenter(dc)
-    source_clusters.append(dc_cluster)
     dest_clusters.append(dc_cluster)
+
+source_clusters = []
+for dc in dcs:
+    dc_cluster = Cluster("dc_cluster")
+    dc_cluster.setCenter(dc)
+    source_clusters.append(dc_cluster)
 
 for p in sourcePoints:
     candidateCluster = source_clusters[0]
@@ -61,14 +73,7 @@ for p in destinationPoints:
             candidateCluster = c
     candidateCluster.addPoint(p)    
 
-map_obj = getMapWithDcs(dcs, tehran_bbox)
-drawClusters(p2p_clusters, map_obj, name="p2p")
-
-map_obj = getMapWithDcs(dcs, tehran_bbox)
-drawClusters(source_clusters + dest_clusters, map_obj, name="cluster")
-
-map_obj = getMapWithDcs(dcs, tehran_bbox)
-drawClusters(source_clusters, map_obj, name="source_cluster")
-
-map_obj = getMapWithDcs(dcs, tehran_bbox)
-drawClusters(dest_clusters, map_obj, name="dest_cluster")
+drawClustersWithDcs("p2p", p2p_clusters, dcs, tehran_bbox)
+drawClustersWithDcs("clusters", source_clusters+dest_clusters, dcs, tehran_bbox)
+drawClustersWithDcs("source_clusters", source_clusters, dcs, tehran_bbox)
+drawClustersWithDcs("dest_clusters", dest_clusters, dcs, tehran_bbox)
