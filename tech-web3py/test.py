7d33ae0b1a16d3968b1a20032f044613c9bf109b
Amir H. Rassafi <amir.h.rassafi@gmail.com>
2023-06-08 13:49:46 +0330

    New Action

--- tech-web3py/test.py
+++ tech-web3py/test.py
@@ -9,4 +9,9 @@ web3 = Web3(Web3.HTTPProvider(eth_url))
 
 print(web3.is_connected())
 
-print(web3.eth.get_balance('0x742d35Cc6634C0532925a3b844Bc454e4438f44e'))
+value = web3.eth.get_balance('0x742d35Cc6634C0532925a3b844Bc454e4438f44e')
+
+print(web3.from_wei(value, 'ether'))
+
+
+print(web3.eth.get_transaction('0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060'))
